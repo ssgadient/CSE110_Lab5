5da4616a324d3dc0bde231c70a826f719de026b8
ssgadient <sgadient@ucsd.edu>
2024-05-07 02:09:40 -0700

    fixes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -27,31 +27,31 @@ function init() {
 
   hornSelect.addEventListener('change', function() {
     if (this.value == "air-horn") {
-      hornImage.src = "/assets/images/air-horn.svg";
-      audio.src = "/assets/audio/air-horn.mp3";
+      hornImage.src = "./assets/images/air-horn.svg";
+      audio.src = "./assets/audio/air-horn.mp3";
     }
     else if (this.value == "car-horn") {
-      hornImage.src = "/assets/images/car-horn.svg";
-      audio.src = "/assets/audio/car-horn.mp3";
+      hornImage.src = "./assets/images/car-horn.svg";
+      audio.src = "./assets/audio/car-horn.mp3";
     }
     else {
-      hornImage.src = "/assets/images/party-horn.svg";
-      audio.src = "/assets/audio/party-horn.mp3";
+      hornImage.src = "./assets/images/party-horn.svg";
+      audio.src = "./assets/audio/party-horn.mp3";
     }
   })
 
   volumeSlider.addEventListener('input', function() {
     if (volumeSlider.value == 0) {
-      volumeImage.src = "/assets/icons/volume-level-0.svg";
+      volumeImage.src = "./assets/icons/volume-level-0.svg";
     }
     else if (volumeSlider.value < 33) {
-      volumeImage.src = "/assets/icons/volume-level-1.svg";
+      volumeImage.src = "./assets/icons/volume-level-1.svg";
     }
     else if (volumeSlider.value < 66) {
-      volumeImage.src = "/assets/icons/volume-level-2.svg";
+      volumeImage.src = "./assets/icons/volume-level-2.svg";
     }
     else {
-      volumeImage.src = "/assets/icons/volume-level-3.svg";
+      volumeImage.src = "./assets/icons/volume-level-3.svg";
     }
     audio.volume = volumeSlider.value/100;
   }) 
